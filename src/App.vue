49d9704bf502ec46d61ebb5ab27e386bb811becf
Anu1601CS <anuragvns1111@gmail.com>
2018-11-18 03:55:32 +0530

    bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar toggleable="md" type="dark" variant="dark">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand href="#">IPL Data Visualization</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item href="#"><router-link to="/">Home</router-link></b-nav-item>
+          <b-nav-item href="#"><router-link to="/about">About</router-link></b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
@@ -15,6 +21,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body,html {
+  width: 100%;
+  height: 100%;
 }
 </style>
