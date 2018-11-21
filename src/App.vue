73c2b3e67b257639c06535c1b41a7c3dd3313687
Anu1601CS <anuragvns1111@gmail.com>
2018-11-21 18:25:56 +0530

    links

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
       <b-navbar-brand href="#">IPL</b-navbar-brand>
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav>
-          <b-nav-item ><router-link to="/" >Home </router-link></b-nav-item>
-          <b-nav-item ><router-link to="/matches">Matches</router-link></b-nav-item>
+          <b-nav-item :to="{ path: '/' }" >Home</b-nav-item>
+          <b-nav-item :to="{ path: '/matches' }" >Matches</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
